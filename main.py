fa7a5d2ced5d2d451b1984445f75123bea7bdd7e
heyoferu <heyoferu@users.noreply.github.com>
2025-04-25 15:04:09 -0600

    feat: include routers
    Signed-off-by: heyoferu <heyoferu@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # app/main.py
 
 from fastapi import FastAPI
-from routers import users, auth, content
+from routers import books,magazines, podcasts,theses, videos
 
 
 app = FastAPI()
 
 
-app.include_router(users.router)
-app.include_router(auth.router)
-app.include_router(content.router)
+
+app.include_router(books.router)
+app.include_router(magazines.router)
+app.include_router(theses.router)
+app.include_router(podcasts.router)
+app.include_router(videos.router)
+# app.include_router(users.router)
+# app.include_router(auth.router)
+# app.include_router(content.router)
+# app.include_router(fuzzy_search.router)
